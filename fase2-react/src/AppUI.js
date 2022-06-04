00ed45416ffd12261aac7c8a6a93d87f0f50f2a8
boblp <robertolozano0011@gmail.com>
2022-06-03 20:50:16 -0500

    Revert "react context"
    This reverts commit 0a895e0e6f2a9bf802438b746092a31ea6eae6fa.

--- fase2-react/src/AppUI.js
+++ fase2-react/src/AppUI.js
@@ -1,43 +1,65 @@
 import React from 'react';
-import { TodoContext } from './TodoContext';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 
-function AppUI() {
+function AppUI({
+    loading,
+    error,
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setSearchValue,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+}) {
+    
+
     return (
         <React.Fragment>
-            <TodoCounter />
-            <TodoSearch />
-            <TodoContext.Consumer>
-                {({error,
-                    loading,
-                    searchedTodos,
-                    completeTodo,
-                    deleteTodo,
-                }) => (
-                    <TodoList>
-                    {error && <p>Desesperate, hubo un error</p>}         
-                    {loading && <p>Estamos cargando, no desesperes</p>}
-                    {(!loading && !searchedTodos.length) && <p>Crea tu primera tarea</p>}              
+            <TodoCounter 
+            total={totalTodos}
+            completed={completedTodos}
+            />
 
-                    {searchedTodos.map((todo) => (
-                        <TodoItem 
-                        key={todo.text} 
-                        text={todo.text}
-                        completed= {todo.completed}
-                        onComplete={() => completeTodo(todo.text)}
-                        onDelete={() => deleteTodo(todo.text)}
-                        />
-                    ))}
-                    </TodoList>
-                )}
-            </TodoContext.Consumer>
+            <TodoSearch 
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+            />
+
+            {/* Bob corrigió este map, de key, le puso el índice porque siempre es único,
+            ya que antes yo lo tenia como todo.text.. jeje  */}
+            <TodoList>
+            {/* {searchedTodos.map((todo, i) => (
+                <TodoItem 
+                key={i} 
+                text={todo.text}
+                completed= {todo.completed}
+                onComplete={() => completeTodo(i)}
+                onDelete={() => deleteTodo(i)}
+                /> */}
+
+            {error && <p>Desesperate, hubo un error</p>}         
+            {loading && <p>Estamos cargando, no desesperes</p>}
+            {(!loading && !searchedTodos.length) && <p>Crea tu primera tarea</p>}              
+
+                {searchedTodos.map((todo) => (
+                    <TodoItem 
+                    key={todo.text} 
+                    text={todo.text}
+                    completed= {todo.completed}
+                    onComplete={() => completeTodo(todo.text)}
+                    onDelete={() => deleteTodo(todo.text)}
+                    />
+            ))}
+            </TodoList>
+
             <CreateTodoButton />
         </React.Fragment>
-    );
+        );
 }
 
 export { AppUI };
