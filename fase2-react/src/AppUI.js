0a895e0e6f2a9bf802438b746092a31ea6eae6fa
boblp <robertolozano0011@gmail.com>
2022-06-03 20:01:06 -0500

    react context

--- fase2-react/src/AppUI.js
+++ fase2-react/src/AppUI.js
@@ -1,65 +1,43 @@
 import React from 'react';
+import { TodoContext } from './TodoContext';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 
-function AppUI({
-    loading,
-    error,
-    totalTodos,
-    completedTodos,
-    searchValue,
-    setSearchValue,
-    searchedTodos,
-    completeTodo,
-    deleteTodo,
-}) {
-    
-
+function AppUI() {
     return (
         <React.Fragment>
-            <TodoCounter 
-            total={totalTodos}
-            completed={completedTodos}
-            />
+            <TodoCounter />
+            <TodoSearch />
+            <TodoContext.Consumer>
+                {({error,
+                    loading,
+                    searchedTodos,
+                    completeTodo,
+                    deleteTodo,
+                }) => (
+                    <TodoList>
+                    {error && <p>Desesperate, hubo un error</p>}         
+                    {loading && <p>Estamos cargando, no desesperes</p>}
+                    {(!loading && !searchedTodos.length) && <p>Crea tu primera tarea</p>}              
 
-            <TodoSearch 
-            searchValue={searchValue}
-            setSearchValue={setSearchValue}
-            />
-
-            {/* Bob corrigió este map, de key, le puso el índice porque siempre es único,
-            ya que antes yo lo tenia como todo.text.. jeje  */}
-            <TodoList>
-            {/* {searchedTodos.map((todo, i) => (
-                <TodoItem 
-                key={i} 
-                text={todo.text}
-                completed= {todo.completed}
-                onComplete={() => completeTodo(i)}
-                onDelete={() => deleteTodo(i)}
-                /> */}
-
-            {error && <p>Desesperate, hubo un error</p>}         
-            {loading && <p>Estamos cargando, no desesperes</p>}
-            {(!loading && !searchedTodos.length) && <p>Crea tu primera tarea</p>}              
-
-                {searchedTodos.map((todo) => (
-                    <TodoItem 
-                    key={todo.text} 
-                    text={todo.text}
-                    completed= {todo.completed}
-                    onComplete={() => completeTodo(todo.text)}
-                    onDelete={() => deleteTodo(todo.text)}
-                    />
-            ))}
-            </TodoList>
-
+                    {searchedTodos.map((todo) => (
+                        <TodoItem 
+                        key={todo.text} 
+                        text={todo.text}
+                        completed= {todo.completed}
+                        onComplete={() => completeTodo(todo.text)}
+                        onDelete={() => deleteTodo(todo.text)}
+                        />
+                    ))}
+                    </TodoList>
+                )}
+            </TodoContext.Consumer>
             <CreateTodoButton />
         </React.Fragment>
-        );
+    );
 }
 
 export { AppUI };
